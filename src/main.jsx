d6efe9a34852dae4bbd95463b4e7dd744ee0ed6e
tristan-ordr <tristan@ordr.io>
2025-08-27 08:31:42 -0600

    Getting react router to work with project.

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,20 @@ import App from './Components/App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import {createBrowserRouter, RouterProvider} from "react-router";
 
-const router = createBrowserRouter([{
-
-}])
+const router = createBrowserRouter([
+    {
+        path: '/e-plantShopping',
+        Component: App
+    }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+        <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
 )
