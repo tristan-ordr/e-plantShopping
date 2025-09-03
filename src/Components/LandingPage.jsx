dcd348c2571d546a365cef575a5c996939e23198
tristan-ordr <tristan@ordr.io>
2025-09-03 12:21:03 -0600

    Using tailwind for the landing page.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,26 +2,34 @@ import React from "react";
 import AboutUs from "./AboutUs.jsx";
 import {Link} from "react-router";
 
-
-export default function() {
+const LandingContent = () => {
     return (
         <>
-            <div className="background-image"></div>
-            <div className="content">
-                <div className="landing_content">
-                    <h1>Welcome To Paradise Nursery</h1>
-                    <div className="divider"></div>
-                    <p>Where Green Meets Serenity</p>
+            <h1 className="text-white text-5xl font-bold">Welcome To Paradise Nursery</h1>
+            <div className="w-[50px] h-[2px] bg-[#4caf50] my-[20px]"></div>
+            <p className="text-white text-2xl">Where Green Meets Serenity</p>
 
-                    <Link
-                        className="get-started-button"
-                        to="shopping"
-                    >
-                        Get Started
-                    </Link>
+            <Link
+                className="mt-[40px] px-[25px] py-[15px] rounded-md bg-[#4caf50] text-white cursor-pointer text-2xl hover:bg-[#45a049]"
+                to="shopping"
+            >
+                Get Started
+            </Link>
+        </>
+    )
+}
+export default function() {
+    return (
+        <div className="relative w-screen h-screen">
+            <div className="bg-[url('https://cdn.pixabay.com/photo/2017/07/13/08/59/greenhouse-2499758_1280.jpg')] absolute top-[0] left-[0] w-[100%] h-[100%] brightness-80 bg-cover"></div>
+            <div className="content flex z-1 h-screen w-screen items-center backdrop-blur-xs bg-[rgba(0,0,0,0.5)] justify-between">
+                <div className="basis-1/3 text-center items-center ml-2 flex flex-col max-w-[350px]">
+                    <LandingContent />
                 </div>
-                <AboutUs/>
+                <div className="basis-2/3">
+                    <AboutUs/>
+                </div>
             </div>
-            </>
+        </div>
     )
 }
