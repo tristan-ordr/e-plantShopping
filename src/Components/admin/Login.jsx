e4fd5fb165a966b109115acfadf95df716f8a6bc
tristan-ordr <tristan@ordr.io>
2025-09-04 11:18:27 -0600

    Using actions to redirect after login.

--- src/Components/admin/Login.jsx
+++ src/Components/admin/Login.jsx
@@ -1,56 +1,18 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from "react-router";
-import axios from "axios";
+import React from 'react';
+import { Form } from "react-router";
 
 
 export default function Login() {
-    const navigate = useNavigate();
-
-    const [user, setUser] = useState({
-        username: "",
-        password: ""
-    })
-
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setUser(prevState=> ({
-            ...prevState,
-            [name]: value
-        }));
-    }
-
-    const handleSubmit = () => {
-        axios.post('http://localhost:4000/login', {
-            username: user.username,
-            password: user.password
-        })
-        .then(res => {
-            if (res.status === 200) {
-                console.log("login success! redirecting...")
-                // login
-                // 1. save our auth token and our refresh token
-
-                // 2. redirect to the logged in page.
-                // TODO - replace this with actions and the `redirect` API. useNavigate is *not* recommended!
-
-                navigate("/e-plantShopping/admin/home");
-
-
-            } else {
-                console.log(res);
-            }
-        })
-        .catch(err =>
-            console.log(err)
-        )
-    }
-
     return (
         <main className="relative w-screen h-screen">
             <div className="bg-[url('https://cdn.pixabay.com/photo/2017/07/13/08/59/greenhouse-2499758_1280.jpg')] absolute top-[0] left-[0] w-[100%] h-[100%] brightness-80 bg-cover"></div>
             <div className="flex flex-col z-1 h-screen w-screen items-center backdrop-blur-xs bg-[rgba(0,0,0,0.5)] justify-center">
-                <section className="bg-white rounded-lg mt-4 flex flex-col px-20 py-8">
-                    <form className="flex flex-col">
+                <Form
+                    action="/e-plantShopping/admin/login"
+                    method="post"
+                    className="flex flex-col">
+
+                    <section className="bg-white rounded-lg mt-4 flex flex-col px-20 py-8">
                         <div className="flex flex-col mt-6">
                             <label
                                 htmlFor="username-input"
@@ -64,8 +26,6 @@ export default function Login() {
                                 type="text"
                                 placeholder="Username"
                                 name="username"
-                                value={user.username}
-                                onChange={handleChange}
                             />
                         </div>
                         <div className="flex flex-col mt-6 mb-6">
@@ -81,22 +41,15 @@ export default function Login() {
                                 type="text"
                                 placeholder="Password"
                                 name="password"
-                                value={user.password}
-                                onChange={handleChange}
                             />
                         </div>
-                    </form>
-                </section>
-                {/*<Link*/}
-                {/*    to="/e-plantShopping/admin/home"*/}
-                {/*    className="mt-10 px-[25px] py-[15px] rounded-md bg-[#4caf50] text-white cursor-pointer text-2xl hover:bg-[#45a049]">*/}
-                {/*    Login*/}
-                {/*</Link>*/}
-                <button
-                    onClick={() => handleSubmit()}
-                    className="mt-10 px-[25px] py-[15px] rounded-md bg-[#4caf50] text-white cursor-pointer text-2xl hover:bg-[#45a049]">
-                    Login
-                </button>
+                    </section>
+                    <button
+                        type="submit"
+                        className="mt-10 px-[25px] py-[15px] rounded-md bg-[#4caf50] text-white cursor-pointer text-2xl hover:bg-[#45a049]">
+                        Login
+                    </button>
+                </Form>
             </div>
         </main>
     )
--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, redirect} from "react-router";
 import Root from "./Components/Root.jsx";
 import LandingPage from "./Components/welcome/LandingPage.jsx";
 import Shopping from "./Components/shopping/Shopping.jsx";
@@ -8,6 +8,7 @@ import Admin from "./Components/admin/Admin.jsx";
 import Login from "./Components/admin/Login.jsx";
 import Transactions from "./Components/admin/Transactions.jsx";
 import Inventory from "./Components/admin/Inventory.jsx";
+import axios from "axios";
 
 const shoppingRouter = createBrowserRouter([
     {
@@ -41,8 +42,38 @@ const shoppingRouter = createBrowserRouter([
             {
                 path: 'admin',
                 children: [
-                    { index: true, Component: Login },
-                    { path: 'home', Component: Admin},
+                    {
+                        index: true,
+                        Component: Login,
+                    },
+                    {
+                        path: 'login',
+                        Component: Login,
+                        action: async ({request}) => {
+                            let formData = await request.formData();
+
+                            let formObj = Object.fromEntries(formData);
+
+                            try {
+                                const response = await axios.post('http://localhost:4000/login', {
+                                    username: formObj.username,
+                                    password: formObj.password
+                                })
+                                if (response.status === 200) {
+                                    return redirect(`/e-plantShopping/admin/home`);
+                                } else {
+                                    console.log(response);
+                                    return redirect(`/e-plantShopping/admin/`);
+                                }
+                            } catch (err) {
+                                console.log(err);
+                                return redirect(`/e-plantShopping/admin/`);
+                            }
+
+                        }
+                    },
+
+                    { path: 'home', Component: Admin },
                     { path: 'inventory', Component: Inventory},
                     { path: 'transactions', Component: Transactions},
                 ]
