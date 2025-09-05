9be40910880c288398e1b6513222b3ff1b10dae2
tristan-ordr <tristan@ordr.io>
2025-09-05 11:15:16 -0600

    Apollo client tutorial

--- src/Components/admin/Locations.jsx
+++ src/Components/admin/Locations.jsx
@@ -1,11 +1,16 @@
 import React, {useEffect} from "react";
 
 // Import everything needed to use the `useQuery` hook
-import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
+import {ApolloClient, gql, HttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloProvider, useQuery} from "@apollo/client/react";
 
 
-export default function Locations() {
+const client = new ApolloClient({
+    link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
+    cache: new InMemoryCache(),
+});
+
+function DisplayLocations() {
     const GET_LOCATIONS = gql`
         query GetLocations {
             locations {
@@ -17,10 +22,32 @@ export default function Locations() {
         }
     `;
 
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.locations.map(({ id, name, description, photo }) => (
+        <div key={id}>
+            <h3>{name}</h3>
+            <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+            <br />
+            <b>About this location:</b>
+            <p>{description}</p>
+            <br />
+        </div>
+    ));
+}
+
+export default function Locations() {
+    // In reality, we'd want the ApolloProvider to be much higher in the tree (wrapping <App/> or similar)
+    //    but for this tutorial, I want everything in one place, and I want to provide a different client further up.
     return (
         <div className="px-10 py-10 w-[100%] h-full">
             <div className="bg-white px-10 py-5 rounded-xl  h-full ">
-                <h1>Hi from locations</h1>
+                <ApolloProvider client={client}>
+                    <DisplayLocations/>
+                </ApolloProvider>
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router";
-import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client/react";
 
 import shoppingRouter from "./router.js";
 
-const client = new ApolloClient({
-    link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
-    cache: new InMemoryCache(),
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ApolloProvider client={client}>
             <RouterProvider router={shoppingRouter}/>
-        </ApolloProvider>
     </React.StrictMode>,
 )
