c9d0f5d53964a76db49dbf8bf02b5386a8653a12
tristan-ordr <tristan@ordr.io>
2025-09-09 15:40:44 -0600

    Added new plant input.

--- src/Components/admin/PlantList.tsx
+++ src/Components/admin/PlantList.tsx
@@ -8,17 +8,22 @@ import NewLabelOutlinedIcon from '@mui/icons-material/NewLabelOutlined';
 import NewCategoryDialog from "./NewCategoryDialog.js";
 import {GetPlantsQuery} from "../../types/generated/schema";
 import EditPlantDialog from "./EditPlantDialog";
+import {useEffect, useRef} from "react";
 
 export default function PlantList() {
     // State
     const [showNewCategory, setShowNewCategory] = React.useState(false);
+
     const [showNewPlant, setShowNewPlant] = React.useState(false);
+    const [insertPlant, setInsertPlant] = React.useState({
+        name: "",
+        cost: "",
+        description: ""
+    })
 
     // TODO - Combine showEditPlant and editPlantId to 1 object:
     const [showEditPlant, setShowEditPlant] = React.useState(false);
     const [editPlantId, setEditPlantId] = React.useState(null);
-
-
 
     // Derived values
 
@@ -26,10 +31,26 @@ export default function PlantList() {
     const hideBackButton = true
 
     // Event handlers
-    const handleNewPlant = () => {
-        console.log('new plant clicked');
-        setShowNewPlant(true);
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>)  => {
+        const {name, value} = event.target;
+
+        setInsertPlant(prevState=> ({
+            ...prevState,
+            [name]: value
+        }));
     }
+
+
+    // useEffect hooks:
+    useEffect( () => {
+        if (showNewPlant) {
+            const divElement = document.getElementById('plant-data-grid');
+            divElement.scrollTop = divElement.scrollHeight + 40;
+
+            const nameInput = document.getElementById('insert-plant-name-input')
+            nameInput.focus();
+        }
+    }, [showNewPlant])
 
     const handleTableRowClicked = (plantId: string) => {
         setShowEditPlant(true);
@@ -99,6 +120,7 @@ export default function PlantList() {
                     <div className="pt-1"></div>
                     <div className="flex flex-col justify-between flex-grow mt-5">
                         <div
+                            id="plant-data-grid"
                             className="grid grid-cols-[minmax(0,1fr)] max-h-[360px]  overflow-x-auto flex-grow rounded-lg border border-gray-200">
                             <table className="w-[100%] border-collapse select-auto">
                                 <thead className="bg:white hover:bg-gray-200 sticky top-0">
@@ -131,10 +153,32 @@ export default function PlantList() {
                                         key="new-plant"
                                     >
                                         <td></td>
-                                        <td className="p-2">input name</td>
+                                        <td className="p-2">
+                                            <input
+                                                id="insert-plant-name-input"
+                                                type="text"
+                                                name="name"
+                                                value={insertPlant.name}
+                                                onChange={handleInput}
+                                            />
+                                        </td>
                                         <td className="p-2">input image</td>
-                                        <td className="p-2">input cost</td>
-                                        <td className="p-2">input description</td>
+                                        <td className="p-2">
+                                            <input
+                                                type="text"
+                                                name="cost"
+                                                value={insertPlant.cost}
+                                                onChange={handleInput}
+                                            />
+                                        </td>
+                                        <td className="p-2">
+                                            <input
+                                                type="text"
+                                                name="description"
+                                                value={insertPlant.description}
+                                                onChange={handleInput}
+                                            />
+                                        </td>
                                         <td className="p-2 pr-4">input category</td>
                                     </tr>
                                 }
@@ -144,7 +188,7 @@ export default function PlantList() {
                         <div className="space-x-3 flex justify-end pt-2">
                             { !showNewPlant &&
                                 <button
-                                    onClick={() => {handleNewPlant()}}
+                                    onClick={() => {setShowNewPlant(true)}}
                                     className="group/button flex items-center justify-center border transform transition-transform duration-50 active:scale-95 focus:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50 bg-transparent border-pink-200 text-pink-500 dark:text-pink-700 hover:bg-pink-100 hover:border-pink-200 disabled:bg-transparent disabled:border-pink-200 disabled:text-pink-700 focus-visible:ring-pink-600 focus-visible:bg-pink-100 h-[34px] py-1.5 px-3 rounded-md text-sm leading-5 space-x-2 add-row mb-3">
                                     <span className="inline-block">Add plant</span>
                                 </button>
@@ -152,6 +196,7 @@ export default function PlantList() {
                             {showNewPlant &&
                                 <>
                                     <button
+                                        onClick={() => setShowNewPlant(false)}
                                         className="group/button flex items-center justify-center border transform transition-transform duration-50 active:scale-95 focus:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50 bg-gray-100 border-gray-200 text-foreground hover:bg-gray-200 hover:border-gray-300 disabled:bg-gray-100 disabled:border-gray-200 focus-visible:ring-gray-600 h-[42px] py-2 px-3 rounded-md text-base leading-6 space-x-3 cancel-insert-row">
                                         <span className="inline-block">Cancel</span>
                                     </button>
