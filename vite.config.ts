4b092e1587d5d23efc0bc9ee77df52422541b69a
tristan-ordr <tristan@ordr.io>
2025-09-12 14:43:40 -0600

    updated vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [
       react(),
       tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  }
 })
