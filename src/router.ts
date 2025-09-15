c2f36510ba53967e4d42356f7ad1dbd2eb08b951
tristan-ordr <tristan@ordr.io>
2025-09-15 11:43:31 -0600

    Started implementing the amplify authenticator component.

--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,8 @@ import Shopping from "./Components/shopping/Shopping.js";
 import Dashboard from "./Components/admin/Dashboard.js";
 import ProductList from "./Components/shopping/ProductList";
 import CartItem from "./Components/shopping/CartItem";
-import Admin from "./Components/admin/Admin";
 import Inventory from "./Components/admin/Inventory.jsx";
+import Admin from "./Components/admin/Admin.tsx";
 
 const router = createBrowserRouter([
     {
